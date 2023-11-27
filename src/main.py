0203906bb9d7ac9ec7dc6ac5aaf7937c07e6ec20
Fscalais <127421223+Fscalais@users.noreply.github.com>
2023-11-27 19:31:56 +0100

    Nouvelle boucle pour pouvoir continuer la simulation plusieurs fois

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from simulation.colony import AntColony
 from gui import run_simulation_gui
 
 def run_simulation_cli(ant_colony, simulation_time):
-    #ant_colony = AntColony()
 
     for _ in range(int(simulation_time)):
         ant_colony.simulate_time_passing(1)
@@ -16,23 +15,26 @@ def run_simulation_cli(ant_colony, simulation_time):
     print(f"Nombre de fourmis: {ant_count}")
 
 def main(ant_colony):
-    parser = argparse.ArgumentParser(description='Simulation de gestion de colonie de fourmis')
-    parser.add_argument('--temps_simulation', type=float, help='Temps de simulation')
-    parser.add_argument('--gui', action='store_true', help='Lancer l\'interface graphique')
+    while True:
+        parser = argparse.ArgumentParser(description='Simulation de gestion de colonie de fourmis')
+        parser.add_argument('--temps_simulation', type=float, help='Temps de simulation')
+        parser.add_argument('--gui', action='store_true', help='Lancer l\'interface graphique')
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    #ant_colony = AntColony()
+        if args.gui:
+            run_simulation_gui()
+        else:
+            temps_simulation = input("Combien de temps voulez-vous faire avancer la simulation? ")
+            run_simulation_cli(ant_colony, temps_simulation)
 
-    if args.gui:
-        run_simulation_gui()
-    else:
-        run_simulation_cli(ant_colony, args.temps_simulation)
+        afficher_types_fourmis = input("Afficher les types de fourmis générés pendant la simulation ? (Oui/Non): ").lower()
+        if afficher_types_fourmis == "oui":
+            ant_colony.show_generated_ant_types()
 
-    afficher_types_fourmis = input("Afficher les types de fourmis générés pendant la simulation ? (Oui/Non): ").lower()
-    if afficher_types_fourmis == "oui":
-        ant_colony.show_generated_ant_types()
-
+        continuer_simulation = input("Voulez-vous continuer la simulation? (Oui/Non): ").lower()
+        if continuer_simulation != "oui":
+            break
 
 
 if __name__ == "__main__":
