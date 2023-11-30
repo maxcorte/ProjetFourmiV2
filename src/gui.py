44167efc226b7d05c3f9ebce615503f7f1cb1d93
Sebiche09 <HE202263@students.ephec.be>
2023-11-30 06:01:32 +0100

    avancement enfin

--- src/gui.py
+++ src/gui.py
@@ -1,15 +1,21 @@
 # gui.py
 import pygame
+import math
 from pygame_widgets.textbox import TextBox
 from pygame.locals import QUIT
 from simulation.colony import AntColony
 import random
+import noise
+
 
 def run_simulation_gui(ant_colony):
     pygame.init()
+# --------------------------------------------- Infos screen ------------------------------------------------------
     screen_info = pygame.display.Info()
     WINDOW_WIDTH, WINDOW_HEIGHT = screen_info.current_w, screen_info.current_h
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# --------------------------------------------- Variables globales ------------------------------------------------------
     pygame.display.set_caption("Simulation de Colonie de Fourmis")
     background_color = (34, 139, 34)  # Vert Forêt
     ant_colony.queen.position = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
@@ -17,8 +23,11 @@ def run_simulation_gui(ant_colony):
     font = pygame.font.Font(None, 36)
 
     clock = pygame.time.Clock()
-    running = True
 
+    digging_list = []
+    running = True
+    noise_map = generate_noise_map(WINDOW_WIDTH, WINDOW_HEIGHT, scale=20, octaves=6, persistence=0.5, lacunarity=2.0,
+                                   seed=1)
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -30,6 +39,9 @@ def run_simulation_gui(ant_colony):
         pygame.draw.rect(screen, (0, 0, 0), (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT // 6), 5)
         pygame.draw.rect(screen, (0, 0, 0), (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT), 10)
         pygame.draw.circle(screen, (139, 0, 0), ant_colony.queen.position, 10)
+        for x, y in digging_list:
+            pygame.draw.rect(screen, (255, 255, 255),
+                             (x - 15, y - 15, 30, 30))
 # --------------------------------------------- Légende ------------------------------------------------------
         #reine
         pygame.draw.circle(screen, (139, 0, 0), [WINDOW_WIDTH-140,50], 10)
@@ -41,17 +53,17 @@ def run_simulation_gui(ant_colony):
 
         if new_larva:
             collides = False
-            new_larva_rect = pygame.Rect(new_larva.position[0], new_larva.position[1], 7, 7)
+            new_larva_rect = pygame.Rect(new_larva.position[0], new_larva.position[1], 20, 20)
             for larva in ant_colony.larvae:
-                existing_larva_rect = pygame.Rect(larva.position[0], larva.position[1], 7, 7)
+                existing_larva_rect = pygame.Rect(larva.position[0], larva.position[1], 20, 20)
                 if existing_larva_rect.colliderect(new_larva_rect):
                     collides = True
                     break
             # Si aucune collision n'a été trouvée, ajouter la nouvelle larve à la colonie
             if not collides:
                 new_larva.position = (
-                    random.randint(WINDOW_WIDTH // 2 - 10, WINDOW_WIDTH // 2 + 10),
-                    random.randint(WINDOW_HEIGHT // 2 + 30, WINDOW_HEIGHT // 2 + 60)
+                    random.randint(WINDOW_WIDTH // 2 - 50, WINDOW_WIDTH // 2 + 50),
+                    random.randint(WINDOW_HEIGHT // 2 + 10, WINDOW_HEIGHT // 2 + 80)
                 )
                 ant_colony.add_larva(new_larva)
 
@@ -63,15 +75,112 @@ def run_simulation_gui(ant_colony):
             if larva.age >= larva.time_to_hatch:
                 larvae_to_remove.append(larva)
                 new_ant = larva.hatch()
-                ant_key = new_ant.ant_type
-                ant_colony.dicAnt[ant_key] = (new_ant, larva.position[0], larva.position[1])
+                new_ant.id = larva.id
+                ant_key = new_ant.id
+                ant_colony.dicAnt[ant_key] = (new_ant, larva.position[0], larva.position[1], 0, 0)
 
 # ---------------------------------------------- Fourmis --------------------------------------------------------
         for larva in larvae_to_remove:
             ant_colony.larvae.remove(larva)
-        for ant_key, (ant, x, y) in ant_colony.dicAnt.items():
-            pygame.draw.circle(screen, (0, 0, 255), (x, y), 3)
+        for ant_key, (ant, x, y, count, move) in ant_colony.dicAnt.items():
+            if ant.ant_type == "Male":
+                if not ant.dead:
+                    # Choisir un mouvement aléatoire
+                    if count == 0:
+                        random_move = random.randint(0, 3)
+                        if random_move == 0:
+                            angle = 0  # vers la droite
+                        elif random_move == 1:
+                            angle = math.pi / 2  # vers le haut
+                        elif random_move == 2:
+                            angle = math.pi  # vers la gauche
+                        elif random_move == 3:
+                            angle = 3 * math.pi / 2  # vers le bas
+                        distance = 10
+                        move = angle
+                        count = random.randint(7, 12)
 
+                    if count != 0:
+                        angle = move
+                        distance = 10
+                        count -= 1
+
+                    # Calculer les nouvelles coordonnées en fonction de la direction
+                    new_x = x + distance * math.cos(angle)
+                    new_y = y + distance * math.sin(angle)
+
+                    if not (30 <= new_x <= WINDOW_WIDTH-30 and WINDOW_HEIGHT/6+30 <= new_y <= WINDOW_HEIGHT-30):
+                        # Inverser la direction en ajoutant ou soustrayant π (pi)
+                        angle += math.pi
+                        # Recalculer les nouvelles coordonnées avec la direction inversée
+                        new_x = x + distance * math.cos(angle)
+                        new_y = y + distance * math.sin(angle)
+
+                    color_under_ant = screen.get_at((int(new_x), int(new_y)))
+                    if color_under_ant == (34, 139, 34):
+                        move = angle - math.pi  # Inverser la direction si la couleur sous la fourmi est verte
+                        count = random.randint(7, 12)
+
+                    # Mettre à jour la position de la fourmi
+                    ant_colony.dicAnt[ant_key] = (ant, new_x, new_y, count, move)
+                    pygame.draw.circle(screen, (165, 42, 42), (int(new_x), int(new_y)), 6)
+                else:
+                    pygame.draw.circle(screen, (0, 0, 0), (x, y), 0)
+            elif ant.ant_type == "Nurse":
+
+                pygame.draw.circle(screen, (255,105,180), (x, y), 6)
+            elif ant.ant_type == "Slaver":
+                pygame.draw.circle(screen, (0,0,0), (x, y), 6)
+            elif ant.ant_type == "Slave":
+                pygame.draw.circle(screen, (255,255,0), (x, y), 6)
+            elif ant.ant_type == "Soldier":
+                if not ant.dead:
+                    # Choisir un mouvement aléatoire
+                    if count == 0:
+                        random_move = random.randint(0, 3)
+                        if random_move == 0:
+                            angle = 0  # vers la droite
+                        elif random_move == 1:
+                            angle = math.pi / 2  # vers le haut
+                        elif random_move == 2:
+                            angle = math.pi  # vers la gauche
+                        elif random_move == 3:
+                            angle = 3 * math.pi / 2  # vers le bas
+                        distance = 10
+                        move = angle
+                        count = random.randint(7, 12)
+
+                    if count != 0:
+                        angle = move
+                        distance = 10
+                        count -= 1
+
+                    # Calculer les nouvelles coordonnées en fonction de la direction
+                    new_x = x + distance * math.cos(angle)
+                    new_y = y + distance * math.sin(angle)
+
+                    if not (30 <= new_x <= WINDOW_WIDTH-30 and WINDOW_HEIGHT/6+30 <= new_y <= WINDOW_HEIGHT-30):
+                        # Inverser la direction en ajoutant ou soustrayant π (pi)
+                        angle += math.pi
+                        # Recalculer les nouvelles coordonnées avec la direction inversée
+                        new_x = x + distance * math.cos(angle)
+                        new_y = y + distance * math.sin(angle)
+
+                    color_under_ant = screen.get_at((int(new_x), int(new_y)))
+
+                    # Utiliser la carte de bruit pour déterminer si la fourmi doit creuser
+                    noise_value = noise_map[int(new_x)][int(new_y)]
+                    digging_threshold = 0.5  # Ajustez ce seuil selon vos besoins
+                    if color_under_ant == (34, 139, 34) and noise_value > digging_threshold:
+                        digging_list.append((int(new_x), int(new_y)))
+
+                    # Mettre à jour la position de la fourmi
+                    ant_colony.dicAnt[ant_key] = (ant, new_x, new_y, count, move)
+                    pygame.draw.circle(screen, (0, 0, 255), (int(new_x), int(new_y)), 8)
+                else:
+                    pygame.draw.circle(screen, (0, 0, 0), (x, y), 0)
+
+        # ---------------------------------------------- Texte --------------------------------------------------------
 
 
 
@@ -82,7 +191,7 @@ def run_simulation_gui(ant_colony):
         screen.blit(ant_text, (50, 135))
 
         pygame.display.flip()
-        clock.tick(1)  # Limite le nombre d'images par seconde
+        clock.tick(60)  # Limite le nombre d'images par seconde
 
     pygame.quit()
 
--- src/simulation/ant.py
+++ src/simulation/ant.py
@@ -1,66 +1,49 @@
 import random
-
+larva_id_count = 0
 class Larva:
-    def __init__(self,x,y):
-        self.__time_to_hatch = random.randint(5, 10)  # Random time for larva to hatch
-        self.__age = 0
+    def __init__(self,x,y,id_larva):
+        self.time_to_hatch = random.randint(5, 10)  # Random time for larva to hatch
+        self.age = 0
         self.position = x,y
-        self.__id = 0
+        self.id = id_larva
 
-    @property
-    def time_to_hatch(self):
-        return self.__time_to_hatch
-    @property
-    def age(self):
-        return self.__age
-
-    @property
-    def id(self):
-        return self.__id
     def ajout_age(self):
-        self.__age += 1
-    @time_to_hatch.setter
+        self.age += 1
+
     def time_to_hatch(self,a,b):
-        self.__time_to_hatch = random.randint(a,b)
+        self.time_to_hatch = random.randint(a,b)
     def hatch(self):
         ant_type = random.choice(["Male", "Nurse", "Slaver", "Slave", "Soldier"])
-        return Ant(ant_type,10,400,400)
+        return Ant(ant_type,10,400,400,0)
 
 class Ant:
-    def __init__(self, ant_type, height, x, y):
+    def __init__(self, ant_type, height, x, y, id_ant):
         self.ant_type = ant_type
-        self.__id = 0
+        self.id = id_ant
         self.__height = height
-        self.__food = 100
-        self.__dead = False
+        self.food = 100
+        self.dead = False
         self.position = x, y
-        self.__age = 0
-    @property
-    def hungry(self):
-        return self.__food
-    @property
-    def dead(self):
-        return self.__dead
-    @hungry.setter
+        self.age = 0
+
     def hungry(self,value):
-        self.__food -= value
-    @dead.setter
+        self.food -= value
+    def eat(self,value):
+        self.food += value
     def dead(self):
-        self.__dead = True
+        self.dead = True
 class Queen(Ant):
     def __init__(self):
-        super().__init__("Queen",10,0, 0)
-        self.__laying_rate = 0.4
+        super().__init__("Queen",10,0, 0,0)
+        self.laying_rate = 0.4
         self.accepted_ants = []
-    @property
-    def laying_rate(self):
-        return self.__laying_rate
-    @laying_rate.setter
-    def laying_rate(self,value):
-        self.__laying_rate = value
+        self.generated_ant_types = []
 
 
+    def set_laying_rate(self,value):
+        self.laying_rate = value
     def lay_eggs(self):
+        global larva_id_count
         """
         .....
 
@@ -68,10 +51,9 @@ class Queen(Ant):
         POST: ?
         """
         if random.random() < self.laying_rate:
-            return Larva(300,300)
-            print("La reine a pondu un œuf.")
-            self.generated_ant_types.append(new_larva.hatch().ant_type)  # Update the list
-            return new_larva
+            larva_id_count += 1
+            return Larva(0,0,larva_id_count)
+
 
 
     def accept_new_ant(self, ant):
@@ -86,27 +68,21 @@ class MaleAnt(Ant):
 class NurseAnt(Ant):
     def __init__(self):
         super().__init__("Nurse")
-        self.__nurse_augment = 0.1
+        self.nurse_augment = 0.1
         # Add any specific attributes or methods for NurseAnt
-    @property
-    def nurse_augment(self):
-        return self.__nurse_augment
-    @nurse_augment.setter
+
     def nurse_augment(self, value):
-        self.__nurse_augment = value
+        self.nurse_augment = value
 
 class SlaverAnt(Ant):
     def __init__(self):
         super().__init__("Slaver")
         # Add any specific attributes or methods for ForagerAnt
-        self.__add_slave = random.randint(1,5)
-        self.__survive_rate = 0.2
-    @property
-    def survive_rate(self):
-        return self.__survive_rate
-    @survive_rate.setter
+        self.add_slave = random.randint(1,5)
+        self.survive_rate = 0.2
+
     def survive_rate(self,value):
-        self.__survive_rate = value
+        self.survive_rate = value
     def go_outside_slaver(self):
         pass
     def come_back_slaver(self):
@@ -117,15 +93,12 @@ class SlaveAnt(Ant):
     def __init__(self):
         super().__init__("Slave")
         # Add any specific attributes or methods for SlaveAnt
-        self.__revolt_rate = 0.05
-    @property
-    def revolt_rate(self):
-        return self.__revolt_rate
-    @revolt_rate.setter
+        self.revolt_rate = 0.05
+
     def revolt_rate(self,value):
-        self.__revolt_rate = value
+        self.revolt_rate = value
     def revolt(self):
-        if random.random() < self.__revolt_rate:
+        if random.random() < self.revolt_rate:
                 pass
 
 
@@ -133,30 +106,15 @@ class SoldierAnt(Ant):
     def __init__(self):
         super().__init__("Soldier")
         # Add any specific attributes or methods for SlaveAnt
-        self.__defense = 0.1
-        self.__exit = False
-        self.__survive_rate= 0.4
-        self.__dig_speed = 0.5
+        self.defense = 0.1
+        self.exit = False
+        self.survive_rate = 0.4
+        self.dig_speed = 0.5
 
-    @property
-    def defense(self):
-        return self.__defense
-    @property
-    def exit(self):
-        return self.__exit
-    @property
-    def survive_rate(self):
-        return self.__survive_rate
-    @property
-    def dig_speed(self):
-        return self.__dig_speed
-
-    @defense.setter
     def defense_modify(self, value):
-        self.__defense = value
-    @survive_rate.setter
+        self.defense = value
     def survive_rate_modify(self,value):
-        self.__survive_rate = value
-    @dig_speed.setter
+        self.survive_rate = value
     def dig_speed_modify(self,value):
-        self.__dig_speed = value
+        self.dig_speed = value
+
